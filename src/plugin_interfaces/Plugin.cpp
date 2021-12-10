ddf28eeec6418a74fa5d0dae94b4f55595fe0a1e
Mm2PL <miau@mail.kotmisia.pl>
2021-12-10 16:55:55 +0100

    Add add basic NetworkRequest capabilities

--- src/plugin_interfaces/Plugin.cpp
+++ src/plugin_interfaces/Plugin.cpp
@@ -3,6 +3,8 @@
 #include <QMessageBox>
 
 #include "Application.hpp"
+#include "common/NetworkRequest.hpp"
+#include "common/Outcome.hpp"
 #include "plugin_interfaces/API.hpp"
 #include "singletons/WindowManager.hpp"
 #include "widgets/Window.hpp"
@@ -21,28 +23,78 @@ namespace plugin_interfaces {
         this->vm.add(
             chaiscript::user_type<CompletionModel::TaggedString::Type>(),
             "TaggedStringType");
+        this->vm.add_global_const(chaiscript::const_var(Outcome(Failure)),
+                                  "Failure");
+        this->vm.add_global_const(chaiscript::const_var(Outcome(Success)),
+                                  "Success");
 
-        auto m = chaiscript::ModulePtr(new chaiscript::Module());
-        using TS = CompletionModel::TaggedString::Type;
-        chaiscript::utility::add_class<TS>(
-            *m, "TaggedStringType",
-            {
-                {TS::Username, "Username"},
-                {TS::EmoteStart, "EmoteStart"},
-                {TS::FFZGlobalEmote, "FFZGlobalEmote"},
-                {TS::FFZChannelEmote, "FFZChannelEmote"},
-                {TS::SEVENTVGlobalEmote, "SEVENTVGlobalEmote"},
-                {TS::SEVENTVChannelEmote, "SEVENTVChannelEmote"},
-                {TS::BTTVGlobalEmote, "BTTVGlobalEmote"},
-                {TS::BTTVChannelEmote, "BTTVChannelEmote"},
-                {TS::TwitchGlobalEmote, "TwitchGlobalEmote"},
-                {TS::TwitchLocalEmote, "TwitchLocalEmote"},
-                {TS::TwitchSubscriberEmote, "TwitchSubscriberEmote"},
-                {TS::Emoji, "Emoji"},
-                {TS::EmoteEnd, "EmoteEnd"},
-                {TS::Command, "Command"},
-            });
-        this->vm.add(m);
+        this->vm.add(chaiscript::user_type<NetworkRequest>(), "NetworkRequest");
+        this->vm.add(chaiscript::constructor<NetworkRequest(
+                         const std::string, NetworkRequestType)>(),
+                     "NetworkRequest");
+        this->vm.add(
+            chaiscript::fun([](NetworkRequest *that, NetworkErrorCallback cb) {
+                return std::move(*that).onError(cb);
+            }),
+            "onError");
+        this->vm.add(chaiscript::fun(
+                         [](NetworkRequest *that, NetworkFinallyCallback cb) {
+                             return std::move(*that).finally(cb);
+                         }),
+                     "finally");
+        this->vm.add(chaiscript::fun([](NetworkRequest *that, int ms) {
+                         return std::move(*that).timeout(ms);
+                     }),
+                     "timeout");
+        this->vm.add(chaiscript::fun([](NetworkRequest *that) {
+                         return std::move(*that).cache();
+                     }),
+                     "cache");
+        this->vm.add(chaiscript::fun(&NetworkRequest::execute), "execute");
+        //// todo: NetworkRequest::header
+        this->vm.add(chaiscript::fun(
+                         [](NetworkRequest *that, NetworkSuccessCallback cb) {
+                             return std::move(*that).onSuccess(cb);
+                         }),
+                     "onSuccess");
+
+        {
+            auto m = chaiscript::ModulePtr(new chaiscript::Module());
+            using TS = CompletionModel::TaggedString::Type;
+            chaiscript::utility::add_class<TS>(
+                *m, "TaggedStringType",
+                {
+                    {TS::Username, "Username"},
+                    {TS::EmoteStart, "EmoteStart"},
+                    {TS::FFZGlobalEmote, "FFZGlobalEmote"},
+                    {TS::FFZChannelEmote, "FFZChannelEmote"},
+                    {TS::SEVENTVGlobalEmote, "SEVENTVGlobalEmote"},
+                    {TS::SEVENTVChannelEmote, "SEVENTVChannelEmote"},
+                    {TS::BTTVGlobalEmote, "BTTVGlobalEmote"},
+                    {TS::BTTVChannelEmote, "BTTVChannelEmote"},
+                    {TS::TwitchGlobalEmote, "TwitchGlobalEmote"},
+                    {TS::TwitchLocalEmote, "TwitchLocalEmote"},
+                    {TS::TwitchSubscriberEmote, "TwitchSubscriberEmote"},
+                    {TS::Emoji, "Emoji"},
+                    {TS::EmoteEnd, "EmoteEnd"},
+                    {TS::Command, "Command"},
+                });
+            this->vm.add(m);
+        }
+        {
+            auto m = chaiscript::ModulePtr(new chaiscript::Module());
+            using ReqTyp = NetworkRequestType;
+            chaiscript::utility::add_class<ReqTyp>(
+                *m, "NetworkRequestType",
+                {
+                    {ReqTyp::Get, "GET"},
+                    {ReqTyp::Post, "POST"},
+                    {ReqTyp::Put, "PUT"},
+                    {ReqTyp::Delete, "DELETE"},
+                    {ReqTyp::Patch, "PATCH"},
+                });
+            this->vm.add(m);
+        }
         try
         {
             this->vm.eval_file(
