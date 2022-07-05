Chatterino/chatterino2

feat: add paints support (#89)
* feat: Merge branch 'feature/paints' into feat/paints

* feat(paints): add class framework to store paints

* refactor(api): move SeventvBadges to SeventvCosmetics to include paints

* refactor(api): clean up SeventvCosmetics

* feat(api): add function to calculate rgba color from decimal

* feat(paints): implement linear-gradient paint

* fix(nametag): remove random example paints

* fix(paints): add LinearGradientPaint constructor

* fix(paints): add prototype radial gradient and url paints implementation

* feat(api): load paints from the 7tv API

* fix(api): remove cv-paste error

* fix(api): store pointers to paints

* refactor(paints): mark asBrush as override and const

* chore(nametag): clean up old imports

* chore(formatting): run clang-format

* fix(compiling): compiling without pch

* refactor: separate classes for 7tv badges and paints

needs 2 API calls because badges are assigned by userID while
paints need the login name

* feat(paints): add method to check if a paint needs to be rendered animated

* refactor(paints): pass the drawing rectangle to asBrush, needed for gradient calculations

* fix(paints): switch out calculation method for linear gradients to correctly translate from css angles

* feat(nametag): paint username elements in chat with the correct paint brush

* chore(formatting): run clang-format

* refactor(api,paints): use QGradientStop(s) typedef

* feat(paints): handle repeating gradients correctly

* fix(paints): support hard edges in gradients

* feat(paints): calculate transparent paints based on the username color

* feat(paints): add radial-gradient paint support -WIP-

* chore(paints): remove TODO comment

* refactor(paints): move helper methods from LinearGradientPaint to parent class

* feat(api): handle transparent colors in radial gradients

* feat(paints): handle repeating radial-gradients

* chore(format): run clang-format

*

chatterino.pro
@@ -211,8 +211,14 @@ SOURCES += \
     src/providers/irc/IrcServer.cpp \
     src/providers/IvrApi.cpp \
     src/providers/LinkResolver.cpp \
+    src/providers/seventv/paints/LinearGradientPaint.cpp \
+    src/providers/seventv/paints/Paint.cpp \
+    src/providers/seventv/paints/PaintDropShadow.cpp \
+    src/providers/seventv/paints/RadialGradientPaint.cpp \
+    src/providers/seventv/paints/UrlPaint.cpp \
     src/providers/seventv/SeventvBadges.cpp \
     src/providers/seventv/SeventvEmotes.cpp \
+    src/providers/seventv/SeventvPaints.cpp \
     src/providers/seventv/SeventvWebSocket.cpp \
     src/providers/twitch/api/Helix.cpp \
     src/providers/twitch/ChannelPointReward.cpp \
@@ -471,8 +477,14 @@ HEADERS += \
     src/providers/irc/IrcServer.hpp \
     src/providers/IvrApi.hpp \
     src/providers/LinkResolver.hpp \
+    src/providers/seventv/paints/LinearGradientPaint.hpp \
+    src/providers/seventv/paints/Paint.hpp \
+    src/providers/seventv/paints/PaintDropShadow.hpp \
+    src/providers/seventv/paints/RadialGradientPaint.hpp \
+    src/providers/seventv/paints/UrlPaint.hpp \
     src/providers/seventv/SeventvBadges.hpp \
     src/providers/seventv/SeventvEmotes.hpp \
+    src/providers/seventv/SeventvPaints.hpp \
     src/providers/seventv/SeventvWebSocket.hpp \
     src/providers/twitch/api/Helix.hpp \
     src/providers/twitch/ChannelPointReward.hpp \

mmattbtw/dankerino

chatterino.pro
@@ -212,8 +212,15 @@ SOURCES += \
     src/providers/irc/IrcServer.cpp \
     src/providers/IvrApi.cpp \
     src/providers/LinkResolver.cpp \
+    src/providers/seventv/paints/LinearGradientPaint.cpp \
+    src/providers/seventv/paints/Paint.cpp \
+    src/providers/seventv/paints/PaintDropShadow.cpp \
+    src/providers/seventv/paints/RadialGradientPaint.cpp \
+    src/providers/seventv/paints/UrlPaint.cpp \
     src/providers/seventv/SeventvBadges.cpp \
     src/providers/seventv/SeventvEmotes.cpp \
+    src/providers/seventv/SeventvPaints.cpp \
+    src/providers/seventv/SeventvWebSocket.cpp \
     src/providers/twitch/api/Helix.cpp \
     src/providers/twitch/ChannelPointReward.cpp \
     src/providers/twitch/IrcMessageHandler.cpp \
@@ -474,6 +481,11 @@ HEADERS += \
     src/providers/irc/IrcServer.hpp \
     src/providers/IvrApi.hpp \
     src/providers/LinkResolver.hpp \
+    src/providers/seventv/paints/LinearGradientPaint.hpp \
+    src/providers/seventv/paints/Paint.hpp \
+    src/providers/seventv/paints/PaintDropShadow.hpp \
+    src/providers/seventv/paints/RadialGradientPaint.hpp \
+    src/providers/seventv/paints/UrlPaint.hpp \
     src/providers/seventv/SeventvBadges.hpp \
     src/providers/seventv/SeventvEmotes.hpp \
     src/providers/twitch/api/Helix.hpp \